b57f8b738d18352078771e807f01d88bc065bc44
André Moraes <andre.carlomoraes@gmail.com>
2023-01-11 20:54:50 -0300

    Add project files.

---
+++ ExemploTentarPush1/Startup.cs
@@ -0,0 +1,21 @@
+﻿Client client = null;
+using (connection)
+{
+    using (var command = new SqlCommand("SELECT * FROM Clients WHERE Id = @id;", connection))
+    {
+        command.Parameters.Add(new SqlParameter("@id", id));
+        connection.Open();
+        using (var reader = command.ExecuteReader())
+        {
+            if (reader.Read())
+            {
+                client = new Client();
+                client.Id = reader.GetString(0);
+                client.Name = reader.GetString(1);
+                client.Email = reader.GetString(2);
+                client.Phone = reader.GetString(3);
+            }
+        }
+    }
+}
+return client;
