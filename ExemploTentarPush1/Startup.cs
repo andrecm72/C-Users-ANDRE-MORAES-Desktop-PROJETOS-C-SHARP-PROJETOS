4e1e577b77c90e9b17d640d769181f0a4f7d0c47
André Moraes <andre.carlomoraes@gmail.com>
2023-01-11 23:14:15 -0300

    Tentativa 2

--- ExemploTentarPush1/Startup.cs
+++ ExemploTentarPush1/Startup.cs
@@ -1,21 +1,59 @@
-﻿Client client = null;
-using (connection)
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SalesWebMvc
 {
-    using (var command = new SqlCommand("SELECT * FROM Clients WHERE Id = @id;", connection))
+    public class Startup
     {
-        command.Parameters.Add(new SqlParameter("@id", id));
-        connection.Open();
-        using (var reader = command.ExecuteReader())
+        public Startup(IConfiguration configuration)
         {
-            if (reader.Read())
-            {
-                client = new Client();
-                client.Id = reader.GetString(0);
-                client.Name = reader.GetString(1);
-                client.Email = reader.GetString(2);
-                client.Phone = reader.GetString(3);
-            }
+            Configuration = configuration;
         }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            _ = services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+
+
     }
 }
-return client;
